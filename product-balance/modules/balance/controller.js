20eb7053af9c2c8c2676547ce183e9b3d3c47dc9
quite2simple <simplechuck882@gmail.com>
2024-09-20 16:41:28 +0300

    Created an outline for balance router and controller

---
+++ product-balance/modules/balance/controller.js
@@ -0,0 +1,16 @@
+exports.getBalanceByFilters = async (filters) => {
+
+};
+
+exports.createBalance = async (productId, shopId, stored, ordered) => {
+
+};
+
+exports.increaseBalance = async (productId, shopId, amount, ordered) => {
+
+};
+
+exports.decreaseBalance = async (productId, shopId, amount, ordered) => {
+
+};
+
---
+++ product-balance/modules/balance/router.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+
+// get balances by filters
+router.get("/", (req, res) => {
+
+});
+
+// create a new balance
+router.post("/", (req, res) => {
+
+});
+
+// increase balance
+router.patch("/increase", (req, res) => {
+    
+});
+
+// decrease balance
+router.patch("/decrease", (req, res) => {
+    
+});
+
