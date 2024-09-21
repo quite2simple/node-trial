e92e345ac35e8acbf75fd8f02ee7a61fa06aedb2
quite2simple <simplechuck882@gmail.com>
2024-09-21 23:25:49 +0300

    Added product router to the app

--- product-balance/index.js
+++ product-balance/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const productRouter = require("./modules/product/router.js");
+const balanceRouter = require("./modules/balance/router.js");
 
 const PORT = process.env.PORT || 5000;
 
@@ -10,6 +12,9 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
+
+app.use("/product", productRouter);
+// app.use("/balance", balanceRouter);
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}`);
