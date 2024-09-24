276d9e25437d7323128f2fd32129faa10b5e9ba1
quite2simple <simplechuck882@gmail.com>
2024-09-24 22:04:23 +0300

    Populated with 1.2M entries

--- user-handler/src/user/user.controller.ts
+++ user-handler/src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 
 @Controller('user')
@@ -9,5 +9,17 @@ export class UserController {
     @Get('count')
     async countAll(): Promise<number> {
         return await this.userService.countAll();
+    }
+
+    @Post('populate')
+    async populateDb() {
+        await this.userService.populateDb();
+        return 'Populated';
+    }
+
+    @Delete('clear')
+    async clearDb() {
+        await this.userService.clearDb();
+        return 'Cleared';
     }
 }
--- user-handler/src/user/user.service.ts
+++ user-handler/src/user/user.service.ts
@@ -1,6 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { User } from './user.model';
+import { faker } from '@faker-js/faker';
+
+interface RandomUserData {
+    firstName: string;
+    lastName: string;
+    age: number;
+    sex: string;
+    problems: boolean;
+}
 
 @Injectable()
 export class UserService {
@@ -14,6 +23,43 @@ export class UserService {
   }
 
   async populateDb() {
+    const data = [];
+    for (let i = 0; i < 12; i++) {
+        for (let j = 0; j < 100_000; j++) {
+            const randomData = this.randomUser();
+            data.push({
+                firstName: randomData.firstName,
+                lastName: randomData.lastName,
+                age: randomData.age,
+                sex: randomData.sex,
+                problems: randomData.problems
+            });
+        }
+        console.log(`${i * 100_000} instances of random user data generated`);
+    }
+    await this.user.bulkCreate(data);
+    console.log(`${data.length} database entries were generated`);
+  }
 
+  async clearDb() {
+    await this.user.destroy({ truncate: true });
+  }
+
+  randomUser(): RandomUserData {
+    return {
+      firstName: faker.person.firstName(),
+      lastName: faker.person.lastName(),
+      age: Math.floor(Math.round(Math.random() * 100)),
+      sex: this.randomSex(),
+      problems: this.randomProblems()
+    };
+  }
+
+  randomSex(): string {
+    return Math.random() > 0.5 ? 'male' : 'female';
+  }
+
+  randomProblems(): boolean {
+    return Math.random() > 0.5;
   }
 }
